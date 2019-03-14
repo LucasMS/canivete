33b247292b73d77f4db53be80d739d126837408a
lsilva <lucmoitinho@gmail.com>
2019-03-14 13:44:08 +0100

    add reference properly

--- R/add.alpha.R
+++ R/add.alpha.R
@@ -6,6 +6,9 @@
 #' @export
 #' @examples
 #' add.alpha("black", 0.5)
+#' @references
+#' add.alpha funciton was made by Markus Gesmann github.com/mages, at http://www.magesblog.com/2013/04/how-to-change-alpha-value-of-colours-in.html
+
 
 
 add.alpha <- function(col, alpha=1){
@@ -15,5 +18,3 @@ add.alpha <- function(col, alpha=1){
         function(x)
           rgb(x[1], x[2], x[3], alpha=alpha))
 }
-
-#add.alpha funciton was made by Markus Gesmann github.com/mages, at http://www.magesblog.com/2013/04/how-to-change-alpha-value-of-colours-in.html
